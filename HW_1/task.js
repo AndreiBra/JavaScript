0d059519481e797fda70b7b9ac39b4a3ff60c0b8
Andrei Bragin <100410326+AndreiBra@users.noreply.github.com>
2022-06-15 22:40:18 +0300

    Update task.js

--- HW_1/task.js
+++ HW_1/task.js
@@ -180,23 +180,23 @@ const checkAge_3 = function(age) {
                 if(+age){
 
         if(age < age_2) {
-            console.log("You don’t have access cause your age is "  + age +  " It’s less then " + age_2)
+            alert("You don’t have access cause your age is "  + age +  " It’s less then " + age_2)
         }
         else if(age>=age_2 && age<age_3) {
-            console.log("Welcome !")
+            alert("Welcome !")
         }
         else if(age>=age_3) {
-            console.log("Keep calm and look Culture channel")
+            alert("Keep calm and look Culture channel")
         }
         else {
-            console.log("Technical work")
+            alert("Technical work")
         }
     }
 else {
-    console.log('Error')}
+    alert('Error')}
 }
 
-let agePromt = prompt('Enter age');
+let agePromt = prompt('Enter your age');
 checkAge_3(agePromt);
 
 checkAge_3 (-13)
