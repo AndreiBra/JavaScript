cc494562286c08af6ff24a82cdd46d3135c8ae39
Andrei Bragin <100410326+AndreiBra@users.noreply.github.com>
2022-06-26 21:38:41 +0300

    Create task_solution_4_stars.js

---
+++ HW_3/task_solution_4_stars.js
@@ -0,0 +1,100 @@
+const company = [
+    {
+      id: 1,
+      name: "Компания",
+      parent: null,
+      users_count: 10,
+      children: [
+        {
+          id: 2,
+          name: "Отдел тестирования",
+          parent: 1,
+          users_count: 7,
+          children: [
+            {
+              id: 2,
+              name: "Тестирование Web",
+              parent: 2,
+              users_count: 5,
+            },
+            {
+              id: 3,
+              name: "Тестирование Mobile",
+              parent: 2,
+              users_count: 0,
+            },
+          ]
+        },
+        {
+          id: 4,
+          name: "Отдел маркетинга",
+          parent: 1,
+          users_count: 30,
+        },
+        {
+          id: 5,
+          name: "Администрация",
+          parent: 1,
+          users_count: 25,
+          children: [
+            {
+              id: 6,
+              name: "Бухгалтерия",
+              parent: 5,
+              users_count: 10,
+            },
+            {
+              id: 7,
+              name: "Менеджмент",
+              parent: 5,
+              users_count: 15,
+              children: [
+                {
+                  id: 8,
+                  name: "Подраздел менеджмента 1",
+                  parent: 7,
+                  users_count: 5,
+                },
+                {
+                  id: 9,
+                  name: "Подраздел менеджмента 2",
+                  parent: 7,
+                  users_count: 10,
+                }
+              ]
+            },
+            {
+              id: 10,
+                 name: "HR",
+                parent: 5,
+              users_count: 1,
+            }
+          ]
+        },
+      ]
+    }
+  ]
+
+
+let multilevel, tree = 0
+let L = ""
+
+function CompanyTree(list) { 
+    if (list.parent < multilevel)
+        tree--
+    if (tree)
+        L = "-".repeat(tree) + " "
+    console.log(L + list.name + " (" + list.users_count + ")")
+    multilevel = list.parent
+    if (list.children) {
+        tree++
+        list.children.forEach(CompanyTree)
+    }
+}
+
+function CompanyStructure(company) {
+    company.forEach(CompanyTree)
+}
+
+CompanyStructure(company)
+
